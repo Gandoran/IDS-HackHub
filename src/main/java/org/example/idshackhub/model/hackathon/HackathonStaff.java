0ba468dd823a472c9e156e8d7f91c13848c0b4f7
gandoran <giakicarloni@gmail.com>
2026-01-20 17:16:04 +0100

    User class added with the Roles and the Assignment for the user

--- src/main/java/org/example/idshackhub/model/hackathon/HackathonStaff.java
+++ src/main/java/org/example/idshackhub/model/hackathon/HackathonStaff.java
@@ -1,5 +1,7 @@
 package org.example.idshackhub.model.hackathon;
 
+import org.example.idshackhub.model.user.User;
+
 public class HackathonStaff {
     private User organizer;
     private User judge;
---
+++ src/main/java/org/example/idshackhub/model/user/role/permission/Permission.java
@@ -0,0 +1,15 @@
+package org.example.idshackhub.model.user.role.permission;
+
+public enum Permission {
+    Can_Create_HackathonTeam,
+    Can_Create_Team,
+    Can_Create_Request,
+    Can_Create_Hackathon,
+    Can_Vote,
+    Can_End_Valutation_State,
+    Can_Submit,
+    Can_Registrate_Team,
+    Can_Manage_Request,
+    Can_Proclamate_Winner,
+    Can_Invite_Judge;
+}
