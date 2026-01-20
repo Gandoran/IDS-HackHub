7a1dcc4d263db136c91271a33d9c4faba9b83a79
gandoran <giakicarloni@gmail.com>
2026-01-20 16:13:15 +0100

    Creazione Progetto

---
+++ src/main/java/org/example/idshackhub/IdsHackHubApplication.java
@@ -0,0 +1,13 @@
+package org.example.idshackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsHackHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdsHackHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/idshackhub/IdsHackHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.idshackhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsHackHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
